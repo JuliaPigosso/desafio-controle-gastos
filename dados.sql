9dafc562bf00726c643cca6f607a571ecddf5261
Julia Pigosso <juliafrancopigosso@gmail.com>
2022-11-20 20:49:00 -0300

    Inserção de transações

--- dados.sql
+++ dados.sql
@@ -1,28 +1,38 @@
 -- Coloque seus scripts de inserção/atualização ou remoção de dados aqui
+-- inserir usuario
 INSERT INTO usuarios (nome,email,objetivo_financeiro)
-VALUES('Giovanni Previatti','Giovanni.Previatti', 'casar');
+VALUES('Walter & Patricia','walter.paty', 'aposentadoria');
 
 SELECT * FROM usuarios;
-
+--inserir conta e atrelar ao usuario
 INSERT INTO contas (nome,instituicao_financeira,cor,usuarios_id)
-VALUES('corrente','Banco Inter', 'Laranja', 2);
+VALUES('corrente','Banco Itau', 'Azul', 6);
 
 SELECT * FROM contas;
-
-INSERT INTO categorias (nome)
-VALUES('mercado');
-
-update categorias SET cor = 'Vermelho', nome = 'Mercado' 
-WHERE id = 1;
+-- inserir categoria
+INSERT INTO categorias (nome, cor)
+VALUES('Compras', 'Prata');
 
 SELECT * FROM categorias;
+--arrumar ou alterar categoria
+update categorias SET nome = 'Vestuario', cor = 'Laranja' 
+WHERE id = 4;
 
+SELECT * FROM categorias;
+--inserir transações
 INSERT INTO transacoes (descricao,valor,data,contas_id,categorias_id)
-VALUES('chocolate',21.90,'03/11/2022',1,1);
+VALUES('Cofrinho',-200,'01/06/2022',4,14);
 
 SELECT * FROM transacoes;
+-- alterar ou arrumar transações
+update transacoes SET valor = 150 
+WHERE id = 40;
 
+SELECT * FROM transacoes;
+--juntar a tabela de transações com a tabela de contas 
 SELECT transacoes.* FROM transacoes
 INNER JOIN contas on contas.id = transacoes.contas_id
 INNER JOIN usuarios on usuarios.id = contas.usuarios_id
 WHERE usuarios.id = 2;
+
+
